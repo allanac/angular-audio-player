7f3aebe478cbfd211453223ab81010e53d8eeddd
iigmir <roc120j@gmail.com>
2017-03-03 09:22:02 +0800

    add random song button

--- player.js
+++ player.js
@@ -9,11 +9,9 @@ function($scope)
         {name:"Der Hölle Rache kocht in meinem Herzen",group:"european",src:"7/7d/Der_Hoelle_Rache.ogg"},
         {name:"The Stars and Stripes Forever",group:"american",src:"5/59/The_Stars_and_Stripes_Forever_-_U.S._Navy_Band.ogg"},
     ];
-    
-    $scope.change_and_play = function()
-    {   // https://stackoverflow.com/questions/30881128/how-to-play-a-sound-in-angularjs
-        // https://stackoverflow.com/questions/29871864/audio-tag-how-to-handle-it-from-angular
-        var audio = new Audio();
-        audio.play();
-    };
+   $scope.shuf_song = function()
+   {
+        var music_id = Math.floor(Math.random() * $scope.list.length );
+        $scope.list_model = $scope.list[music_id];
+   }
 });
